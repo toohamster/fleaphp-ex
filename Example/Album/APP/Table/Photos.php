3bdbe12587426bc7e5e52ebbe7bd151b01974c7c
toohamster <449211678@qq.com>
2026-02-10 10:17:19 +0800

    remove some code

--- Example/Album/APP/Table/Photos.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-FLEA::loadClass('FLEA_Db_TableDataGateway');
-
-class Table_Photos extends FLEA_Db_TableDataGateway
-{
-    var $tableName = 'photos';
-    var $primaryKey = 'photo_id';
-}
--- Example/DynamicMenu/APP/Table/SysMenu.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-FLEA::loadClass('FLEA_Db_TableDataGateway');
-
-class Table_SysMenu extends FLEA_Db_TableDataGateway
-{
-	var $tableName = 'sysmenu';
-	var $primaryKey = 'menu_id';
-}
